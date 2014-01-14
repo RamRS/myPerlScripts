53015e184ac73d7d65951b025a041135e3b76799
Ram RS <ramrs@nyu.edu>
2014-01-14 17:52:56 -0500

    Added Argparse support
    Added argparse implementation to handle command line arguments better

--- filterMulFa.pl
+++ filterMulFa.pl
@@ -2,21 +2,40 @@
 use strict;
 use Bio::Seq;
 use Bio::SeqIO;
+use Getopt::ArgParse;
 
 # This script reads a FASTA file with multiple sequences, a text file with a list 
 # of IDs and picks from the FASTA file sequences that have an ID match in the ID
 # file. These sequences are output to STDOUT
 
-# Inputs - 2 mandatory - FASTA file with input sequences and text file with IDs of 
-# sequences to be picked
-my $inFile=shift;
-my $ids=shift;
+my $argPr = Getopt::ArgParse->new_parser(
+			help => 'extract subset of sequences from FASTA file',
+			description => 'This script reads a FASTA file with multiple \
+			sequences, a text file with a list of IDs and picks from the \
+			FASTA file sequences that have an ID match in the IDs file. \
+			These sequences are then printed to STDOUT'
+			);
 
+# Add arguments to capture input FASTA file and IDs file
+$argPr->add_arg('--fasta','-f',required=>1,help=>'input FASTA file');
+$argPr->add_arg('--ids','-i',required=>1,help=>'file with IDs of sequences to be picked');
+
+# Print usage text on improper usage
+if (scalar(@ARGV) != 2)
+{
+	$argPr->print_usage();
+	exit(1);
+}
+
+# Store input arguments into variables
+my $argArr = $argPr->parse_args();
+my $inFile = $argArr->fasta;
+my $idsFile = $argArr->ids;
 
 # Open files, use BioPerl to create object to read FASTA file and read all IDs
 # into an array
 my $inSeqs = Bio::SeqIO->new(-file => "$inFile", -format => 'fasta' );
-open ID, $ids or die "Could not open filter file";
+open ID, $idsFile or die "Could not open filter file";
 my @idArr = <ID>;
 close ID;
 chomp @idArr;
