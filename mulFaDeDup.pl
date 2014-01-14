713bf2caed2725cc1d024d9bef9a82199140a82b
Ram RS <ramrs@nyu.edu>
2014-01-14 17:40:15 -0500

    Argparse + File handling
    Added argparse support and better file handling

--- mulFaDeDup.pl
+++ mulFaDeDup.pl
@@ -1,31 +1,51 @@
-#!/usr/bin/perl -w
+#!/usr/bin/perl
 use strict;
 use Bio::Seq;
 use Bio::SeqIO;
+use Getopt::ArgParse;
+use File::Basename;
 
 # This script reads a multi-fasta file and splits the input into unique and duplicate
 # sequences. Additionally, found duplicates might be written to a separate file
 
-# Usage text
-sub usage{
-	my $usageText=
-	"This script reads a multi-fasta file and removes duplicate sequences 
-Usage:
-	$0 input_fasta_file file_to_write_uniq_seqs [file_to_write_dup_seqs]";
-	print $usageText;
-}
+my $argPr = Getopt::ArgParse->new_parser(
+			help => 'remove sequence duplicates from FASTA file',
+			description => 'This script reads a multi-fasta file and splits the input \
+			into unique and duplicate sequences. Additionally, found duplicates might \
+			be written to a separate file'
+			);
+			
+# Add arguments to capture input FASTA file and duplicate print flag
+$argPr->add_arg('--fasta','-f',required=>1,help=>'input FASTA file');
+$argPr->add_arg('--dups','-d',type=>'Bool',help=>'flag to set to save duplicates to a separate file');
 
-# Print usage on improper call
-if (scalar(@ARGV) < 2 or scalar(@ARGV) > 3)
+# Print usage text on improper usage
+if (scalar(@ARGV) < 1 or scalar(@ARGV) > 2)
 {
-	usage();
+	$argPr->print_usage();
+	exit(1);
 }
 
-# Files - 2 mandatory (input file and output filename) and 1 optional file to hold any 
-# duplicate sequences found
-my $inFile=shift;
-my $outFile=shift;
-my $dupFile=shift;
+my $argArr = $argPr->parse_args();
+my $inFile;
+
+$inFile = $argArr->fasta;
+unless (-e $inFile) 
+{
+	print "Input file does not exist!";
+	exit(1);	
+}
+my ($fileName,$dirPath,$extn)=fileparse($inFile,qr/\.[^.]*/);
+my $outFile = "${dirPath}${fileName}_uniq${extn}"; 
+print "Writing unique sequences to $outFile";
+
+my $dupFlagOn = $argArr->dups();
+my $dupFile;
+if ($dupFlagOn)
+{
+	$dupFile = "${dirPath}${fileName}_dups${extn}";
+	print "Writing duplicate sequences to $dupFile";
+}
 
 # Open files, use BioPerl to read and write fasta sequences
 my $inSeqs = Bio::SeqIO->new(-file => "$inFile", -format => 'fasta' );
@@ -33,7 +53,7 @@ my $uniqSeqs = Bio::SeqIO->new(-file => ">$outFile", -format => 'fasta' );
 
 # Define object for duplicate file only if file for duplicates is given as input
 my $dupSeqs;
-if(defined $dupFile)
+if($dupFlagOn)
 { 
 	$dupSeqs = Bio::SeqIO->new(-file => ">$dupFile", -format => 'fasta' ); 
 }
@@ -47,7 +67,7 @@ while(my $seqRec=$inSeqs->next_seq())
         my $sequence = $seqRec->seq();
 		
 		# If sequence exists in hash, write entry to dups file
-        if(exists $seqIdHash{$sequence} && defined $dupFile)
+        if(exists $seqIdHash{$sequence} && $dupFlagOn)
         {
         	$dupSeqs->write_seq($seqRec);
         }
