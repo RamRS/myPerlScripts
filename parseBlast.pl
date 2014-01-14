bf7e786f361f272cce81f8701ab85c2052b689fa
Ram RS <ramrs@nyu.edu>
2014-01-14 18:08:22 -0500

    Added argparse support
    Added argparse support to handle command line arguments better

--- parseBlast.pl
+++ parseBlast.pl
@@ -4,31 +4,37 @@ use Bio::Seq;
 use Bio::SeqIO;
 use Bio::SearchIO;
 use Bio::SearchIO::Writer::TextResultWriter;
+use Getopt::ArgParse;
 
 # This script reads a BLAST results file as input and filters out query
 # sequences with no hits to the database.
 
-# Define usage
-sub usage()
-{
-	my $usage = "$0 <BLAST_results_file> <output_file_name>";
-	print $usage;
-}
+my $argPr = Getopt::ArgParse->new_parser(
+			help => 'remove entries with no hits from BLAST output file',
+			description => 'This script reads a BLAST results file as input\
+			 and filters out query sequences with no hits to the database. \
+			 The results are written in plain text format to output file.'
+			);
+
+# Add arguments to capture input FASTA file
+$argPr->add_arg('--fasta','-f',required=>1,help=>'input FASTA file');
 
 # Print usage on improper call
-if(scalar(@ARGV) != 2)
+if(scalar(@ARGV) != 1)
 {
-	usage();
+	$argPr->print_usage();
+	exit(1);
 }
 
 # Read the BLAST results file
 $\="\n";
-my $in = shift;
+my $argArr = $argPr->parse_args();
+my $inFile = $argArr->fasta;
 my $out = shift;
 
 # Create BioPerl objects to read BLAST results file and an outputformatter
 # object to format output before writing it to output file
-my $blast = Bio::SearchIO->new( -file => "$in", -format => 'blast');
+my $blast = Bio::SearchIO->new( -file => "$inFile", -format => 'blast');
 my $writer = new Bio::SearchIO::Writer::TextResultWriter();
 my $filt = new Bio::SearchIO(-file => ">$out", -writer => $writer);
 
