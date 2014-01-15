4e2c1901e0a55fd29fbfc93bf98fc53aa7caf93f
Ram RS <ramrs@nyu.edu>
2014-01-14 19:12:32 -0500

    Argparse support
    Added argparse support to process CMD line arguments better

--- pickLenMtoN.pl
+++ pickLenMtoN.pl
@@ -2,18 +2,40 @@
 use strict;
 use Bio::Seq;
 use Bio::SeqIO;
+use Getopt::ArgParse;
 
 # This script filters a FASTA file and picks sequences with length between given
 # inputs. It also reports the number of sequences that fall below the lower
 # bound, above the upper bound and between the two bounds
 
-# Three required inputs - input file with all sequences, min length and max 
-# length. Output written to STDOUT
-my $inFile = shift; 
+my $argPr = Getopt::ArgParse->new_parser(
+			help => 'pick sequences in the given length range from input \
+			FASTA file',
+			description => 'This script filters a FASTA file and prints \
+			to STDOUT sequences with length between given inputs. It \
+			also reports the number of sequences that fall below the \
+			lower bound, above the upper bound and between the two bounds.'
+			);
 
 # Variables to hold min and max values
-my $minLen = shift;
-my $maxLen = shift;
+
+
+# Add arguments to capture input FASTA file, min length and max length
+$argPr->add_arg('--fasta','-f',required=>1,help=>'input FASTA file');
+$argPr->add_arg('--min','-l',required=>0,default=>0,help=>'minimum length to filter with. Default: 0');
+$argPr->add_arg('--max','-u',required=>0,default=>10000,help=>'maximum length to filter with. Default: 10,000');
+
+if(scalar(@ARGV) < 1 or scalar(@ARGV) > 3)
+{
+	$argPr->print_usage();
+	exit(1);
+}
+
+# Parse CMD line args to variables
+my $argArr = $argPr->parse_args();
+my $inFile = $argArr->fasta;
+my $minLen = $argArr->min;
+my $maxLen = $argArr->max;
 
 # BioPerl objects for the input file
 my $inSeqs = Bio::SeqIO->new(-file => "$inFile", -format => 'fasta');
@@ -31,18 +53,18 @@ while ( my $seqRec = $inSeqs->next_seq())
         if ($seqLen >= $minLen && $seqLen <= $maxLen)
         {
         		# If length is between min and max, write to output
-		my $seqId = $seqRec->id();
-		print "\n>$seqId";
-		print $seqRec->sequence();
+				my $seqId = $seqRec->id();
+				print "\n>$seqId";
+				print $seqRec->sequence();
 
                 # counter increment for length distribution calculation
                 $cIn++;
         }
-	elsif ($seqLen < $minLen) $cBelow++
-	else $cAbove++
+		elsif ($seqLen < $minLen) { $cBelow++; }
+		else { $cAbove++; }
 }
 # Length distribution reporting
-print "Length distribution:\n"
+print "Length distribution:\n";
 print "Below $minLen:\t$cBelow\n";
 print "Between $minLen and $maxLen:\t$cIn\n";
 print "Above $maxLen:\t$cAbove\n";
